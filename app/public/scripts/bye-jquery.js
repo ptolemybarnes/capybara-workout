52d0cb0f7fe9cc449590a5db87384c62bd90b144
Roi Driscoll <roi.driscoll@gmail.com>
2022-01-13 11:43:45 +0100

    Enhances checkbox section

--- app/public/scripts/bye-jquery.js
+++ app/public/scripts/bye-jquery.js
@@ -1,11 +1,22 @@
 const allButtons = document.querySelectorAll("button")
+const allCheckboxes = document.querySelectorAll("input[type=checkbox]")
 
 if(allButtons) {
-    console.log(allButtons);
     allButtons.forEach(button => {
         button.addEventListener("click", () => {
             button.classList.add('clicked');
             button.innerHTML = "You clicked me!"
+        });
+    })
+};
+
+if(allCheckboxes) {
+    allCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                const label = document.querySelector(`label[for="${checkbox.name}"]`)
+                label.innerHTML = `<input name="${checkbox.name}" type="checkbox" checked>You clicked me!`;
+            }
         });
     })
 };
